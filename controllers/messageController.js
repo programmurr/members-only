dd53c24fce754313164f91b5368d7f14a2f7a257
conor <conorjm89@gmail.com>
2022-01-11 23:04:44 +0000

    Add create message view

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,1 +1,3 @@
 const Message = require("../models/Message");
+
+exports.message_create_post = async function (req, res, next) {};
--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const passport = require("passport");
 require("dotenv").config();
 
 const userController = require("../controllers/userController");
+const messageController = require("../controllers/messageController");
 
 router.get("/", (req, res) => {
   res.render("index", { page: "Members Only", user: req.user });
@@ -16,7 +17,7 @@ router.get("/sign-up", (req, res) => {
 
 router.post("/sign-up", userController.user_create_post);
 
-//LOG IN ROUTES
+// LOG IN ROUTES
 router.get("/log-in", (req, res) => {
   res.render("logIn", { page: "Log In", message: req.flash("error") });
 });
@@ -54,5 +55,7 @@ router.post("/join-club", userController.user_update_membership_post);
 router.get("/create-message", (req, res) => {
   res.render("createMessage", { page: "Write a Message" });
 });
+
+router.post("/create-message", messageController.message_create_post);
 
 module.exports = router;
